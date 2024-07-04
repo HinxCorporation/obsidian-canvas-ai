bfbc1a9ad0e2cf74e83f8cc8023060367986f817
chilam <chilam.Lee@outlook.com>
2024-07-04 17:39:32 +0800

    :bug: fix: 添加再次追问的textNode，并优化返回ai回复node

--- chat.ts
+++ chat.ts
@@ -32,7 +32,7 @@ export default class ChatCanvasExtension {
             // 创建textNode
             let height = node.height;
             let y = node.y + height + 30;
-            const newTextNode = node.canvas.createTextNode({
+            const answerTextNode = node.canvas.createTextNode({
               pos: { x: node.x, y },
               text: "Loading...",
               size: { width: node.width, height },
@@ -47,7 +47,7 @@ export default class ChatCanvasExtension {
               fromSide: "bottom",
               fromNode: node.id,
               toSide: "top",
-              toNode: newTextNode.id,
+              toNode: answerTextNode.id,
             }
 
             const canvasData = node.canvas.getData();
@@ -58,29 +58,47 @@ export default class ChatCanvasExtension {
             // 提交prompt到ai服务，获取返回json数据
             this.createRequest(node.text, (message: string) => {
               if (message === '[DONE]') {
-                let newTextNodeData = newTextNode.getData();
-                newTextNodeData.height += 10;
-                // 更新textNode的高度
-                newTextNode.setData(newTextNodeData);
+                let answerTextNodeData = answerTextNode.getData();
+                answerTextNodeData.height += 10;
+                // 更新answerTextNode的高度
+                answerTextNode.setData(answerTextNodeData);
                 node.canvas.requestSave();
+                // 添加追问的TextNode
+                const askTextNode = node.canvas.createTextNode({
+                  pos: { x: answerTextNode.x, y: answerTextNode.y + answerTextNode.height + 30 },
+                  text: "",
+                  focus: true,
+                })
+                // 添加连接线
+                const askEdge = {
+                  id: randomUUID().replace(/-/g, '').toString().substring(0, 16),
+                  fromSide: "bottom",
+                  fromNode: answerTextNode.id,
+                  toSide: "top",
+                  toNode: askTextNode.id,
+                }
+                const canvasData = node.canvas.getData();
+                canvasData.edges.push(askEdge);
+                node.canvas.setData(canvasData);
                 return;
               }
 
               // 返回的token
+              console.log(message);
               const token = JSON.parse(message).choices[0].delta.content;
               text += (token);
-              newTextNode.setText(text);
+              answerTextNode.setText(text);
 
-              // 获取newTextNode的Data
-              let newTextNodeData = newTextNode.getData();
-              // 获取newTextNode的高度
-              let oldHeight = newTextNodeData.height;
+              // 获取answerTextNode的Data
+              let answerTextNodeData = answerTextNode.getData();
+              // 获取answerTextNode的高度
+              let oldHeight = answerTextNodeData.height;
               // 获取文本高度（包括不可见高度）
-              let height = newTextNode.contentEl.firstChild.firstChild.scrollHeight;
+              let height = answerTextNode.contentEl.firstChild.firstChild.scrollHeight;
               if (oldHeight < height) {
-                newTextNodeData.height = height;
+                answerTextNodeData.height = height;
                 // 更新textNode的高度
-                newTextNode.setData(newTextNodeData);
+                answerTextNode.setData(answerTextNodeData);
               }
             });
           });
